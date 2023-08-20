eb2665519dd90f54f3d2ff5b9a57c1cf3bff7e55
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-20 20:59:09 +0500

    changes

--- js-outputs.js
+++ js-outputs.js
@@ -4,3 +4,8 @@ document.write(2+4);
 window.alert("developer boy");
 console.log('waheed');
 
+let x = 10;
+let y = 21;
+let z = x + y;
+document.getElementById("getValue").innerHTML =
+ "the value of x and y is " + z + ".";
