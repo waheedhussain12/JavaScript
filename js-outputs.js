e8646c6dfee24d8b6a860edce04c32e2d2e98128
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-21 03:08:24 +0500

    variables

--- js-outputs.js
+++ js-outputs.js
@@ -1,7 +1,7 @@
 document.getElementById("access-Element").innerHTML= "ADD HTML DATA USING innerHTML";
 
 document.write(2+4);
-window.alert("developer boy");
+// window.alert("developer boy");
 console.log('waheed');
 
 let x = 10;
@@ -9,3 +9,16 @@ let y = 21;
 let z = x + y;
 document.getElementById("getValue").innerHTML =
  "the value of x and y is " + z + ".";
+
+ let number1 , number2;
+ number1 = 20;
+ number2 = 30;
+ let sum = number1 + number2;
+ console.log(sum);
+
+ let number3 , number4;
+ number3 = 20;
+ number4 = 30;
+ let sum1 = number3 * number4;
+ console.log(sum1);
+
