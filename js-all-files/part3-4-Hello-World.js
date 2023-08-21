5e28a47378172dc22014273d553017917d18973f
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-22 00:36:37 +0500

    js learning

--- js-all-files/part3-4-Hello-World.js
+++ js-all-files/part3-4-Hello-World.js
@@ -8,3 +8,9 @@ document.write("iam full stack developer");
 // for display data on alert box
 console.log("hy my name is waheed");
 // for debugging purpose
+
+// statment and comments part 4
+
+var number; //declare variable
+number = 10; // assign value in number variable
+console.log(number); 
