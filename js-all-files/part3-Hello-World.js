895f40544827f31dc7ed69896b20221ccb77b9c5
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-21 20:36:15 +0500

    different types of output

---
+++ js-all-files/part3-Hello-World.js
@@ -0,0 +1,10 @@
+// different types of output
+// alert("hy");
+// to access an html element use this access element by id and class
+document.getElementById("get").innerHTML = "waheed laghari";
+
+// for testing purpose 
+document.write("iam full stack developer");
+// for display data on alert box
+console.log("hy my name is waheed");
+// for debugging purpose
