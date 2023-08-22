636470127738b71d8f62a152d0448b14004279e7
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-22 14:18:12 +0500

    variables

---
+++ js-all-files/part5-JavaScript-Variables.js
@@ -0,0 +1,9 @@
+// three different ways define a variables in js
+// var let const
+
+var number; // declare
+number = 10; // put a value in number 
+// console.log(number); print
+var name1 = "waheed";
+var name1 = "ali"
+console.log(name1);
