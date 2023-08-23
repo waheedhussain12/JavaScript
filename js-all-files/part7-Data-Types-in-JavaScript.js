ed214492d1e6777f9fd6a3932f4e41f1cefbccd5
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-24 02:16:51 +0500

    data types of javascript

---
+++ js-all-files/part7-Data-Types-in-JavaScript.js
@@ -0,0 +1,45 @@
+// string
+let x = "waheed'Laptop";
+// reason for using "" and '' quote
+let y = 'hussain"housse';
+console.log(x , y);
+let z = "waheed\"mobile"; //" use double quote in text same way with single quote
+console.log(z);
+let a = 'my name\'waheed'
+console.log(a);
+
+// number
+let num = 34;
+console.log(num);
+let num1 = 2312.12;
+console.log(num1);
+// boolean true or flase
+let c = 23;
+let d = 10;
+console.log(c>d);
+let p = 23 > 10;
+console.log(p);
+
+// array
+let arr = [ "waheed", 23, "hussain", 23.23];
+console.log(arr);
+
+// object
+let obj = {yuName:"waheed hussain", Age: 23, Caste: "leghari"};
+console.log(obj);
+
+let def;
+console.log(def); // undefined 
+let undef = ""; // this is not undefined 
+console.log(undef);
+
+let firstName = typeof"waaheed";
+console.log(firstName);
+let firstName1 = "waaheed";
+console.log(firstName1);
+
+let noName = "hussain"; // known data type 
+console.log(typeof noName);
+
+let buy = "chaii";
+console.log(`thank for the ${buy}`);
