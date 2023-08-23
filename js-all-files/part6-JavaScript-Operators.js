321ba833eb67828dc5fbc7e6169f24be52c050e5
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-24 01:31:29 +0500

    arithmetic operations

---
+++ js-all-files/part6-JavaScript-Operators.js
@@ -0,0 +1,66 @@
+// addition
+let num = 20;
+let num2 = 40;
+let result = num + num2;
+console.log(result);
+
+//sub
+
+let sub = 50;
+let sub2 = 40;
+let result1 = sub - sub2;
+console.log(result1);
+
+// multiple
+let multiple = 10;
+let multiple2 = 30;
+let result2 = multiple * multiple2;
+console.log(result2);
+
+//divide
+
+let divide = 20;
+let divide2 = 34;
+let result3 = divide / divide2;
+console.log(result3);
+
+// remider
+let remider = 33;
+let remider2 = 34;
+let result4 = remider % remider2
+console.log(result4);
+let a = 10;
+a += 3; // addition
+console.log(a);
+
+// power 
+let power = 5;
+let result5 = power **2;
+console.log(result5);
+
+let number = 5; // number
+let strng = "55"; // string
+
+console.log(number+strng);
+// number convet in string
+
+let yuName = "waheed";
+let yuName2 = "hussain";
+console.log(yuName + " "+ yuName2);
+
+// i++ or i--
+let incrmnt = 10;
+incrmnt++; // add 1
+incrmnt++; // add 1
+incrmnt++; // add 1
+console.log(incrmnt);
+
+
+let incrmnt1 = 10;
+incrmnt1--; // - 1
+incrmnt1++; // + 1
+console.log(incrmnt1);
+
+let s = 5;
+let result6 = (s + 5) / 5; // first yu addition put ()
+console.log(result6);
