f8a12c145787fa7ded993a02fd535b2d64539ea2
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-25 01:47:22 +0500

    done

---
+++ js-all-files/practice.js
@@ -0,0 +1,8 @@
+// different types of outputs in js
+// alert('hy waheed');
+// console.log("waheed hussain");
+// document.write("hussain");
+
+// access element through id
+
+document.getElementById("getme").innerHTML = " html id now in javascript page";
