cf0afda0f2ae04fcd03e3868e00e642dd219095d
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-25 02:03:19 +0500

    var done

--- js-all-files/practice.js
+++ js-all-files/practice.js
@@ -18,4 +18,27 @@ var num1 = 23; // value update in same variable
 console.log(num1);
 
 var  person = "waheed hussain", age = 23, caste = "laghari";
-// 
+// different datatypes in one var
+console.log(person,age,caste);
+
+
+//  var is not a block scope bcz yu can access outside of  the block scope
+
+{
+var num2 = 55;
+var num3 = 20;
+var z = num2 + num3;
+// console.log(z); 
+}
+// can we access from here outside of the block scope
+console.log(z); // yes output is 75 so var is not a block scope
+
+// automaticaly declare variable in js
+
+auto = "yes iam auto";
+auto1 = 100;
+auto3 = auto + auto1;
+console.log(auto3);
+// 100 number convert in string 
+
+// let
