0a608e29e6061a5d71d7e947ef789d5ecf2644fb
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-25 01:55:08 +0500

    variables

--- js-all-files/practice.js
+++ js-all-files/practice.js
@@ -5,4 +5,17 @@
 
 // access element through id
 
-document.getElementById("getme").innerHTML = " html id now in javascript page";
+// document.getElementById("getme").innerHTML = " html id now in javascript page";
+
+// variables var let const
+var num; // declare
+num = 12; // value assign
+console.log(num);
+
+var num1 = 34;
+num1 = "waheed"; // value update
+var num1 = 23; // value update in same variable
+console.log(num1);
+
+var  person = "waheed hussain", age = 23, caste = "laghari";
+// 
