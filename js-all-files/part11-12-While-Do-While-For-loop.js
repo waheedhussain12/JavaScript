822a0e21f5e0317ba90fb174efd445e220e35916
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-27 00:50:10 +0500

    while loop

--- js-all-files/part11-12-While-Do-While-For-loop.js
+++ js-all-files/part11-12-While-Do-While-For-loop.js
@@ -21,32 +21,61 @@
 // }
 
 
-let counter1  = 1;
-let sum = 0;
-while (counter1 <=10) {
-    if (counter1 % 2 ==0) {
-        sum = sum + counter1
-    }
-    counter1++;
-    document.write(sum);
+// let counter1  = 1;
+// let sum = 0;
+// while (counter1 <=10) {
+//     if (counter1 % 2 ==0) {
+//         sum = sum + counter1
+//     }
+//     counter1++;
+//     document.write(sum);
+// }
+
+// do while
+// let counter2 = 12;
+// // do does not care if its first condition is false
+// do {
+//     document.write('waheed');
+//     counter2 ++;
+    
+// } while (counter2 <=10);
+
+// let counter3 = 12;
+// do {
+//     document.write("print  me");
+//     counter3 ++;
+    
+// } while (counter3 <= 10)
+
+
+
+// for loop
+
+// for( let counter4 = 1; counter4 <=10; counter4 ++){
+    
+//     document.write('print using for loop in js');
+// }
+
+let  numberA = 1;           
+
+while (numberA <=20) {
+    console.log("print 1 to 20",numberA);
+    numberA++;
+    
 }
 
-let counter2 = 12;
-// do does not care if its first condition is false
-do {
-    document.write('waheed');
-    counter2 ++;
-    
-} while (counter2 <=10);
+// let numberB = 1;
 
-let counter3 = 12;
-do {
-    document.write("print  me");
-    counter3 ++;
-    
-} while (counter3 <= 10)
+// let priintName = "add this";
 
-for( let counter4 = 1; counter4 <=10; counter4 ++){
-    
-    document.write('print using for loop in js');
-}
+// while (numberB <= 10) {
+
+//     if( numberB ==6){
+//         console.log(priintName);
+//         // break;
+//      // if we use break program out of the loop
+//     }
+  
+//   console.log(numberB);
+//   numberB++;  
+// }
