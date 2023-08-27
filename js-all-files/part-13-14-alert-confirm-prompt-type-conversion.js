608d58cdd74e23d566fe99316484a8eaa7a028c8
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-27 20:55:48 +0500

    commit

--- js-all-files/part-13-14-alert-confirm-prompt-type-conversion.js
+++ js-all-files/part-13-14-alert-confirm-prompt-type-conversion.js
@@ -4,6 +4,27 @@
 // show a message.
 
 
+// let age = prompt(`Enter Your age ` ,22);
+// // for storing user entering data make variable and puts on it
+// // document.write(age); 
+// // display on scrren
+// if (age != null) {
+//     document.write(`Your Age is ${age}`);
+     
+    
+// }else{
+//     document.write(`Age Field was Blank`);
+// }
 
 
+// confrim function
 
+let response = confirm(`Are You Sure, You want to Delete`);
+// check response true or false
+if (response) {
+    document.write(`Deleted`);
+    
+    
+}else{
+    document.write(`Not Deleted`);
+}
