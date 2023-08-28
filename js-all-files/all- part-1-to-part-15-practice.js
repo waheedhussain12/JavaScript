89b77c3df1f26c008893e1c83f2174c9c421a07b
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-29 01:52:15 +0500

    practices

---
+++ js-all-files/all- part-1-to-part-15-practice.js
@@ -0,0 +1,30 @@
+// three output functions
+// alert("hy baby");
+
+// let age;
+
+// age = prompt('enter your age');
+
+// if (age <=100) {
+//     document.write(`Your age is ${age}`);
+    
+// } else{
+
+//     document.write('enter age btw 100 numbers');
+// }
+
+
+// let response = confirm(`Are You Sure, You want to Delete`);
+// // check response true or false
+// if (response) 
+// // if response is true print this
+// {
+
+//     document.write(`Deleted`);
+    
+    
+// }else{
+//     document.write(`Not Deleted`);
+// }
+
+
