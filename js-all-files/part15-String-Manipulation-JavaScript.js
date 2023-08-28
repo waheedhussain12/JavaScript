db0961d1273a930576db8b5d2101f43b4990aebb
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-28 17:29:57 +0500

    String spcaing function

---
+++ js-all-files/part15-String-Manipulation-JavaScript.js
@@ -0,0 +1,57 @@
+// string manipulation
+
+// string in single quote
+
+// let str = 'waheed';
+// console.log(str);
+
+// string in double quote
+
+// let str2 = "hussain";
+// console.log(str2);
+
+// two string plus +
+
+// let str = "waheed";
+// let str2 = "hussain";
+// console.log(str + str2);
+
+// store value another string
+
+// let str = "string one";
+// let str2 = `${str} store in str2`
+// // now str value inside str2
+// console.log(str2);
+
+// // string in new line use \n
+
+// let str = 'waheed\nhussain';
+// console.log(str);
+
+
+// space in sentence in string
+//  use \' \ " put \ in string use double \\ in sentence
+// use \ btw strings and put any letter or sign btw them
+// let str = 'waheed\\hussain';
+// // output waheed\hussain
+// console.log(str);
+
+// let str = 'waheed\"hussain';
+// // output waheed"hussain
+// console.log(str);
+
+// let str = 'waheed\'hussain';
+// // output waheed'hussain
+// console.log(str);
+
+// let str = 'waheed\@hussain';
+// // output waheed@hussain
+// console.log(str);
+
+
+// tab space use t
+
+// let str = 'waheed\thussain';
+// // output waheed"hussain
+// console.log(str);
+
