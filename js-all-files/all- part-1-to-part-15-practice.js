b3f33999a56b73dc2f5d889b6219ce5949ebe04d
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-29 02:27:19 +0500

    now learning js function

--- js-all-files/all- part-1-to-part-15-practice.js
+++ js-all-files/all- part-1-to-part-15-practice.js
@@ -27,6 +27,9 @@
 //     document.write(`Not Deleted`);
 // }
 
+
+// type convrn 
+
 var num = 34;
 console.log(num); // here it is number
 console.log(typeof num);
@@ -34,11 +37,53 @@ console.log(typeof num);
 var num = "34";
 console.log(num); // here it is string
 console.log(typeof num);
+
+
+// numbr + string = num convrt in string 1030
 var num = 10 + '30'; // 10 convert in string 1030
 console.log(num);
 console.log(typeof num);
 
+// numbr + null = 30 and null is 0
 var num = 30 + null;
+console.log(num); // 30 and null is 0
+console.log(typeof num);
 
+// string + null = null convrt in string 49null
+var num = '49' + null;
+console.log(num); // null convrt in string 49null
+console.log(typeof num);
 
+// numbr multiple in alphabats string = NAN
+var num = 12 * 'waheed';
+console.log(num);
+console.log(typeof num);
 
+// numbr multiple string numbrs = numbers
+// same as / - % *
+// only + are adding string
+var num = 33 * '12';
+console.log(num);
+console.log(typeof num);
+
+// both are string = 2325
+var num = '23' + '25';
+console.log(num);
+console.log(typeof num);
+
+// boolean 
+var num = true;
+console.log(typeof num);
+
+// but true or false in string
+// its string not a boolean
+var num = 'true'
+console.log(num); 
+console.log(typeof num);
+
+// js three functions
+//  string() 
+//  number()
+//   boolean ()
+
+var str = 'hi! iam String in JS'
