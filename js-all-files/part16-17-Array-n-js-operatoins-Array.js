27883ae65cd85e3f61e2e70dfa1c1d6e71d2f75b
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-30 03:08:32 +0500

    done

---
+++ js-all-files/part16-17-Array-n-js-operatoins-Array.js
@@ -0,0 +1,42 @@
+// normal declaration
+
+// var book1 = "Math";
+// var book2 = "Physics";
+// var book3 = "Bio";
+// var book4 = "Computer Science";
+
+// console.log(book1);
+
+
+// add multiple data in array
+var book = ["Math", "Physics","Bio","Computer Science"];
+// print full array
+console.log(book);
+// access particular one elemnet use index
+console.log(book[1]);
+console.log(book[3]);
+
+let asset = [1,2,43,546,76,78,987,3,23,123];
+console.log(asset); // full array
+console.log(asset[4]);// 4th element
+
+
+
+
+
+// Array in js
+
+// two types of declaring in Array
+
+var arr = ["waheed", "hussain","leghari",23];
+// print array
+console.log(arr);
+// add element in array
+arr.push("fav-car");
+// print to see changes in array now
+console.log(arr);
+// find lenght of an array
+console.log(arr.length);
+
+// sec way declare an Array
+var arr = new Array("waheed", "hussain","leghari",23)
