f2b061fbce266c04436b0c7da2aa2be98e7e9cc6
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-30 03:16:16 +0500

    upadate value in Array

--- js-all-files/part16-17-Array-n-js-operatoins-Array.js
+++ js-all-files/part16-17-Array-n-js-operatoins-Array.js
@@ -1,10 +1,11 @@
 // normal declaration
 
-// var book1 = "Math";
+// let book1 = "Math";
 // var book2 = "Physics";
 // var book3 = "Bio";
 // var book4 = "Computer Science";
-
+// // update an variable
+// book1 = "sec-year Math"
 // console.log(book1);
 
 
@@ -15,6 +16,11 @@ console.log(book);
 // access particular one elemnet use index
 console.log(book[1]);
 console.log(book[3]);
+//also change or upade value in element using index
+book[0] = "English";
+book[2]= 23; // update number also
+console.log(book);
+
 
 let asset = [1,2,43,546,"Number in Array",76,78,987,3,23,123];
 console.log(asset);  // full array
