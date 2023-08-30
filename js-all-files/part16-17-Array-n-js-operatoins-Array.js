dc3d1421d85ba377910245dc44a1e55e4c3aef83
Waheed Hussain <tommyshelby811@gmail.com>
2023-08-31 01:12:38 +0500

    done

--- js-all-files/part16-17-Array-n-js-operatoins-Array.js
+++ js-all-files/part16-17-Array-n-js-operatoins-Array.js
@@ -22,10 +22,34 @@ console.log(book);
 book[2]= 23; // update number also
 console.log(book);
 
+// find length in Array
+console.log(book.length);
+// add element in array using push() function 
+// using push you can add element only on last
+book.push('Urdu');
+console.log(book);
 
-let asset = [1,2,43,546,"Number in Array",76,78,987,3,23,123];
-console.log(asset);  // full array
-console.log(asset[4]); // 4th element
+// also you can add element in starting of in Array
+// using unshift() function
+book.unshift('add me on Start Chemistry');
+console.log(book);
+
+// also you can delete an element in Array 
+// using pop
+
+
+
+
+
+
+
+
+
+
+
+// let asset = [1,2,43,546,"Number in Array",76,78,987,3,23,123];
+// console.log(asset);  // full array
+// console.log(asset[4]); // 4th element
 
 
 
@@ -40,18 +64,18 @@ console.log(asset[4]); // 4th element
 
 // two types of declaring in Array
 
-var arr = ["waheed", "hussain","leghari",23];
-// print array
-console.log(arr);
-// add element in array
-arr.push("fav-car");
-// print to see changes in array now
-console.log(arr);
-// find lenght of an array
-console.log(arr.length);
-// Delete element using pop() function but delete last element in array
-arr.pop()
-console.log(arr);
+// var arr = ["waheed", "hussain","leghari",23];
+// // print array
+// console.log(arr);
+// // add element in array
+// arr.push("fav-car");
+// // print to see changes in array now
+// console.log(arr);
+// // find lenght of an array
+// console.log(arr.length);
+// // Delete element using pop() function but delete last element in array
+// arr.pop()
+// console.log(arr);
 
-// sec way declare an Array
-var arr = new Array("waheed", "hussain","leghari",23)
+// // sec way declare an Array
+// var arr = new Array("waheed", "hussain","leghari",23)
