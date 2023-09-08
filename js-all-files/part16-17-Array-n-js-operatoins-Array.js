bbb3894b73ed2f1daa20f3f00fd0422ced8f5a5e
Waheed Hussain <tommyshelby811@gmail.com>
2023-09-08 22:03:12 +0500

    done

--- js-all-files/part16-17-Array-n-js-operatoins-Array.js
+++ js-all-files/part16-17-Array-n-js-operatoins-Array.js
@@ -210,3 +210,5 @@ function myFunction(getValues){
 // var arr = new Array("waheed", "hussain","leghari",23)
 // var arr = new Array("waheed", "hussain","leghari",23)
 // var arr = new Array("waheed", "hussain","leghari",23)
+// var arr = new Array("waheed", "hussain","leghari",23)
+// var arr = new Array("waheed", "hussain","leghari",23)
