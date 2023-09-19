cef4bb790007e3402764b532583c23900ec3c969
Waheed Hussain <tommyshelby811@gmail.com>
2023-09-20 01:56:47 +0500

    done

--- js-all-files/part-18-19-20-function.js
+++ js-all-files/part-18-19-20-function.js
@@ -10,7 +10,22 @@
 // user define
 // table of 2
 
-for(i = 1; i < 0; i++){
-    console.log(i*2);
+// for(i = 1; i <= 10; i++){
+//     console.log(`2 x ${i} = ${2*i} ${i/2}`);
+
+// }
+
+// make a function
+function multiTable(){
+    for(i = 1; i <= 10; i++){
+        console.log(`2 x ${i} = ${2*i} ${i/2}`);
+    
+    }
+    
 
 }
+// call function
+// print this table 100h 1000th time 
+// simply call this function
+multiTable();
+multiTable();
