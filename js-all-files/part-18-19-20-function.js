aeabf66ef1f1a9f11b07cbb8621724bd7b4db029
Waheed Hussain <tommyshelby811@gmail.com>
2023-09-22 18:27:24 +0500

    function

--- js-all-files/part-18-19-20-function.js
+++ js-all-files/part-18-19-20-function.js
@@ -81,7 +81,7 @@ addTwoNumber();
 // with parameters and arguments
 function multiTable2(table1) {
   for (var i = 1; i <= 10; i++) {
-    console.log(`2 x ${table1} = ${table1 * i} ${table1 / i}`);
+    console.log(`${table1} X ${i} = ${table1 * i}`);
   }
 }
 // calling function
@@ -170,9 +170,8 @@ storeFunInVar(3, 2); // function store in variable
 // now add values as you want
 addThreeNumbers(4, 6, 2, 35, 46, 56, 76, 76, 56);
 
-
 // return in function
-function addd( a,b){
+function addd(a, b) {
   // also you can make new variable for store data
   let results = a + b;
   // note anything you write in return which in print
@@ -184,45 +183,45 @@ function addd( a,b){
   // console.log(a + b);
 }
 
-// store function's value in variable 
+// store function's value in variable
 // first function execute than return a value
-let sumReturn = addd(10 , 20);
+let sumReturn = addd(10, 20);
 console.log(sumReturn);
 // calling function
 // addd(10 , 5);
 
-
 // compare function
-function compare(a , b){
-if (a > b) {
-  return 1;
-
-} else if(b > a){
-return -2;
-} else{
-  return 0;
-}
+function compare(a, b) {
+  if (a > b) {
+    return 1;
+  } else if (b > a) {
+    return -2;
+  } else {
+    return 0;
+  }
 }
 // make a variable for store return data and print this variable
 let stor;
-stor = compare( 7 , 2);
+stor = compare(7, 2);
 console.log(stor);
- 
+
 // Global variable vs local variable
- 
 
-let car = 'Audi';
+let car = "Audi";
 // global variable
+// let car = 'BMW';
+// got error bcz in global variable you cannot allowed
+// same variable
 
-function add23(){
-   let result = 60; // local variable
-   console.log(car); // global variable
-   console.log(result);
-   // if variable declare inside the function
+function add23() {
+  let result = 60; // local variable
+  console.log(car); // global variable
+  console.log(result);
+  // if variable declare inside the function
   // is called local variable
   // you cannot access this variable out of the function
- // in last line local varible is delete from function
- // that why you can declare same variable different functions
+  // in last line local varible is delete from function
+  // that why you can declare same variable different functions
 }
 add23();
 // console.log(result);
@@ -230,50 +229,14 @@ add23();
 
 console.log(car); // global variable
 
-function subr(){
+function subr() {
   let result = 6034; // local variable
   console.log(result);
   // same variable but different function is allowed
-
+  // note function end variable is also deleted thats why same variable allowed
 }
 // calling function
 subr();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // functions
 function playSonog() {
@@ -313,3 +276,164 @@ function addanythink(add, add2) {
 }
 addanythink(334, 23);
 addanythink("developers", "sindh uni");
+
+// simple function
+function simpleFun() {
+  var twoNum = 34;
+  var twoNum2 = 74;
+  result = twoNum + twoNum2;
+  console.log("Result is ", result);
+}
+// calling function
+simpleFun();
+
+// function with parameters and argumentes
+function funWithParaArgumnt(str1, str2) {
+  // pass parameters
+  // if yu cannot passing the paramets
+  // get undefined
+
+  var str1;
+  var str2;
+  console.log(str1 + str2);
+}
+// calling function
+funWithParaArgumnt("waheed", "hussain");
+// same function but different arguments or values
+funWithParaArgumnt(23, 34);
+
+// function print table
+function printTable() {
+  for (tb = 1; tb <= 10; tb++) {
+    console.log(`2 X ${tb} = ${2 * tb}`);
+  }
+}
+
+// calling function
+printTable();
+
+// using parameters and arguments
+// print any number's table as you want
+// function print any table
+function printAnyTable(anytb, secAnytb) {
+  for (tb = 1; tb <= 10; tb++) {
+    console.log(`${anytb} X ${tb} = ${anytb * tb}`);
+    // this loop print 4th numbr table
+  }
+
+  for (tb1 = 1; tb1 <= 10; tb1++) {
+    console.log(`${secAnytb} X ${tb1} = ${secAnytb * tb1}`);
+    // this loop print 9th numbr table
+  }
+  // but function is same
+}
+// calling function
+printAnyTable(4, 9);
+
+// function with parameters and argument
+// with condition check
+
+function funCondiCheck(topSong) {
+  console.log("TOP 5 ALL TIME HIT SONGS IN THE WORLD");
+  console.log("Select one song using numbers 1 to 5");
+  for (let topTenSong = 0; topTenSong <= topSong; topTenSong++) {
+    if (topSong == 0) {
+      // console.log(topSong);
+      console.log("Try again and select 1 to 5 Songs");
+    }
+
+    if (topTenSong == 1 && topSong == 1) {
+      console.log(topSong);
+      console.log("Tum Ho Pass Mere From RockStar");
+    }
+    if (topTenSong == 2 && topSong == 2) {
+      console.log(topSong);
+      console.log("Bikhra From Abdul hannan");
+    }
+    if (topTenSong == 3 && topSong == 3) {
+      console.log(topSong);
+      console.log("O Re piyaa From Ustad Nusrat");
+    }
+    if (topTenSong == 4 && topSong == 4) {
+      console.log(topSong);
+      console.log("Clam Down From Rema Selena Gomez");
+    }
+    if (topTenSong == 5 && topSong == 5) {
+      console.log(topSong);
+      console.log("Sada Pyaar AP Dhillon");
+    }
+    if (topTenSong == 6 && topSong == 6) {
+      // console.log(topSong);
+      console.log("Select one to five Songs");
+    }
+  }
+}
+funCondiCheck(2);
+
+
+// the Arguments Objects
+
+function withoutParameters(){
+let getargumntlen;
+  getargumntlen = arguments.length;
+  console.log(getargumntlen); // get length
+  console.log(arguments);
+}
+// calling function
+// argument increass length is also increass
+// withoutParameters(23,3); 
+// withoutParameters(23,3,5);
+withoutParameters(23,3,'waheed',98);
+
+// exp 
+function sumfunArgumnt(){
+  if (arguments.length == 0) {
+    console.log('No Arguments are Passed!');
+    
+  } else{
+    var sum = 0;
+  for(i = 0; i < arguments.length; i++){
+    sum = sum + arguments[i];
+// sum for store values 
+  }
+  console.log(sum);
+  }
+  }
+
+  sumfunArgumnt(2 ,4,1,2);
+
+
+  // return function
+  function returnValue(num1 , num2){
+var num1 , num2;
+sum = num1 + num2;
+return sum;
+
+
+  }
+  // calling function
+  // note in return Store function in a variable
+  // not return value
+  let resultw =returnValue(12,31) ;
+  console.log(resultw);
+
+
+
+// global variable
+
+let globalVar = 'waheed hussain';
+console.log(globalVar);
+
+// trying to access local variable
+// console.log(cannotAcess); 
+// got error
+
+function localVar(){
+  let cannotAcess = 'local variable can access its own function';
+  console.log(cannotAcess);// access its own function only
+  console.log(globalVar);// global var access anywhere
+}
+// calling function
+localVar();
+
+function islogin(){}
