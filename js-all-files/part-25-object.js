e9c47352b94a67db9e91163a25e215405d333677
Waheed Hussain <tommyshelby811@gmail.com>
2023-09-24 01:34:37 +0500

    Object and its properties

--- js-all-files/part-25-object.js
+++ js-all-files/part-25-object.js
@@ -3,74 +3,71 @@
 // var name = {} object
 // var person = {}; // empty object
 // console.log(person);
-
+// person is object
 var person = {
-firstName: "waheed",
-lastName:"hussain"
-}; 
+  firstName: "waheed", // these are propertyies
+  lastName: "hussain",
+};
 
-
-// two ways in access an object 
+// two ways in access an object
 
 // first way access from . notation
-console.log(person.firstName,person.lastName);
+console.log(person.firstName, person.lastName);
 
 // sec way to access object form like Array[] notation
-console.log(person['firstName']); // access like Array 
-console.log(person['lastName']);
+console.log(person["firstName"]); // access like Array
+console.log(person["lastName"]);
 // console.log(person); // print all object
 
+var person = {
+  // space is allow in properties like first Name
+  // note use ""or '' double quote or single than inside property name
+  "first Name": "waheed", // note Space are count use camel case or -
+  lastName: "hussain",
+};
+// here .notation is not working
+console.log(person["first Name"]); // this is allow
+console.log(person.lastName); // this is also allowed
 
 var person = {
-    // space is allow in properties like first Name
-    // note use ""or '' double quote or single than inside property name
-    'first Name': "waheed", // note Space are count use camel case or - 
-    lastName:"hussain"
-    }; 
-    // here .notation is not working 
-    console.log(person['first Name']); // this is allow
-    console.log(person.lastName); // this is also allowed
+  firstName: "waheed",
+  lastName: "hussain",
+};
+// also print full object
+console.log(person);
+// also add property later
+person.age = 33; // here person is object and age is proprty and 33 is property's value
+// Also you can change value in property
+person.lastName = "Ali";
+console.log(person.lastName);
+// Also you Can delete property in object
+delete person.lastName;
+console.log(person); // now lastName is deleted
+// What if we are not asign property in object
+// output is undefined
+console.log(person.age);
 
+// Also You can check in object this property is exist or not
+console.log(person.favcolor);
+// You will get undefined bcz this property is not exits in object
+// Also sec way to check
+console.log("favcolor" in person);
+// Here property inside the stirng
+// In opertor give us value in true or false
 
-    var person = {
-        firstName: "waheed",
-        lastName:"hussain"
-        }; 
-        // also print full object
-        console.log(person);
-        // also add property later
-    person.age = 33;
-    // Also you can change value in property
-    person.lastName = "Ali";
-    console.log(person.lastName);
-    // Also you Can delete property in object
-    delete person.lastName;
-    console.log(person); // now lastName is deleted
-        // What if we are not asign property in object
-        // output is undefined 
-        console.log(person.age);
+// We don't Know How maney properties  are in object
+// So We can use for in loop
+var person = {
+  firstName: "waheed",
+  lastName: "hussain",
+};
+// also print full object
+console.log(person);
+// also add property later
+person.age = 33;
 
-        // Also You can check in object this property is exist or not
-        console.log(person.favcolor);
-        // You will get undefined bcz this property is not exits in object
-        // Also sec way to check
-        console.log('favcolor' in person);
-        // Here property inside the stirng 
-        // In opertor give us value in true or false
-
-        // We don't Know How maney properties  are in object
-        // So We can use for in loop
-        var person = {
-            firstName: "waheed",
-            lastName:"hussain"
-            }; 
-            // also print full object
-            console.log(person);
-            // also add property later
-        person.age = 33;
-
-        for(let check in person){
-            // console.log(check); // show all property in output
-            console.log(check + ": " + person[check]); // also check values of 
-        // using index[] like Array
-        }
+for (let check in person) {
+  // console.log(check); // show all property in output
+  console.log(check + ": " + person[check]); // also check values of
+  // using index[] like Array
+}
