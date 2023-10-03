2cdcd3e42d91cad82883ea88c3b0440afcc0f8d5
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-04 02:21:12 +0500

    done

---
+++ js-all-files/practicee-part3-.js
@@ -0,0 +1,17 @@
+// topic String
+
+var str = 'waheed hussain';
+var str2 = 'leghari';
+console.log(str);
+
+var store = str ;
+console.log(store);
+
+var addStr = str.concat(` ${str2}`);
+console.log(addStr);
+
+strSpace = 'this is my\"laptop';
+console.log(strSpace);
+
+
+
