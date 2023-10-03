fe507c92913aac1dbbde9c21d788ba4428db2f50
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-04 02:38:43 +0500

    done

--- js-all-files/practicee-part3-.js
+++ js-all-files/practicee-part3-.js
@@ -11,7 +11,29 @@ var addStr = str.concat(` ${str2}`);
 console.log(addStr);
 
 strSpace = 'this is my\"laptop';
-console.log(strSpace);
+console.log(strSpace.length);
+console.log(strSpace[4]);
 
+var addtwoStr = str + str2;
+console.log(addtwoStr);
+
+if (str == "waheed hussain") {
+    console.log('this is true');
+}else if(str2 == 'leghari'){
+console.log('this is also true');
+}else{
+    console.log('got error');
+}
+
+var pra = "this is my lovely dog ";
+console.log(pra);
+var checkByword = pra.indexOf('my');
+console.log(checkByword);
+var checkByNumber = pra.indexOf('my',3);
+console.log(checkByNumber);
+var includesStr = pra.includes('this');
+console.log(includesStr);
+var replaceStr = pra.replace('my','MY')
+console.log(replaceStr);
 
 
