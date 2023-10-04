dcde24b411b840fee089d29591cd37191826c2fe
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-05 00:14:23 +0500

    done

--- Dom-start/part-1-dom.js
+++ Dom-start/part-1-dom.js
@@ -28,7 +28,7 @@ js can add new HTML elements and attributes
 js can react to all existing HTML events in the page
 js can create new HTML events in the page
 
-Note : in the DOM, all HTML Elements are defined as (OBJECTS). so it will
+Note : In the DOM, all HTML Elements are defined as (OBJECTS). so it will
 have Both Property and Method
 
 
