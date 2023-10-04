eafe7227f1777be0aa1f962f097b1db37ff19e48
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-04 23:59:27 +0500

    done

---
+++ Dom-start/part-1-dom.js
@@ -0,0 +1,44 @@
+// Document Object Model (DOM)
+
+ /* the Document Object Model (DOM) is an application programmming
+ interface (API) for manipulating HTML and XML documents
+ (ADD , REMOVE , and Modify parts of the documents or HTML)
+ 
+ Manipulate means change in HTML documment add delete update
+    
+ change in html document through js using DOM  
+ 
+ when a web page is loaded the browser creates a document object model of  the page
+ 
+Note in this DOM tree , the document is the root or node or object
+
+
+Note Document is the parent or ROOT  of the DOM TREE
+
+id or class for identify element
+
+with the object model, javaScript gets all the power it needs to create dynamic HTML
+
+
+js can change all the HTML elements in the page
+js can change all the HTML attributes in the page
+js can change all the CSS styles in the page
+js can remove existing HTML elements and attributes
+js can add new HTML elements and attributes
+js can react to all existing HTML events in the page
+js can create new HTML events in the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
