ee7f6c9513bde8eabbf88eedc09875a106811eea
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-05 01:54:21 +0500

    done

---
+++ Dom-start/part-39-select-an-Element-by-ID.js
@@ -0,0 +1,12 @@
+
+
+
+var store = document.getElementById("first");
+// if id is wrong return nulll
+// here Parent object is Document
+// here .getElementById() ==> is Method
+// this is object's Method
+// for store all data make new variable and store it
+// innerHTML is property
+store.innerHTML = "FirstName: Waheed Hussain";
+console.log(store);
