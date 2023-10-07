da82d08907da2baf866de2ba3171b578ae2ae37c
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-08 01:31:04 +0500

    done

--- Dom-start/practice-39-to-44.js
+++ Dom-start/practice-39-to-44.js
@@ -58,3 +58,4 @@ console.log(mainDivsh2tag);
 // here inside the main div' all  h2 is selected
 // here inside the main div' all  h2 is selected 
 // here inside the main div' all  h2 is selected 
+// here inside the main div' all  h2 is selected 
