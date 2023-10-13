dd8d58798c423d408491403ba3b11d2885c42954
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-14 00:58:15 +0500

    done

--- Dom-start/practice-manipulating-html-elements.js
+++ Dom-start/practice-manipulating-html-elements.js
@@ -250,7 +250,3 @@ let key2 = adjacentElement.getAttribute("newClass");
 console.log(key2);
 let key3 = adjacentElement.hasAttribute("newClass");
 console.log(key3);
-let key3 = adjacentElement.hasAttribute("newClass");
-console.log(key3);
-let key3 = adjacentElement.hasAttribute("newClass");
-console.log(key3);
