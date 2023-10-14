ebb04d228e8441645849866de4c5714ad64f2fe5
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-15 02:45:09 +0500

    done

--- Dom-start/part-42-Query-Selector.js
+++ Dom-start/part-42-Query-Selector.js
@@ -1,16 +1,16 @@
-// query selector select element like css
-var selectElement = document.querySelector("p.intro");
-// here if two same class in div querySelector Apply only first Element
-selectElement.innerHTML = 'if two or more classes are same query selector select first element';
-console.log(selectElement);
+// // query selector select element like css
+// var selectElement = document.querySelector("p.intro");
+// // here if two same class in div querySelector Apply only first Element
+// selectElement.innerHTML = 'if two or more classes are same query selector select first element';
+// console.log(selectElement);
 
-// if you select all same class element use query selector
-// use query selectorAll 
-var selectAllElement = document.querySelectorAll("h1.intro");
-// here if two same class in div querySelector Apply only first Element
-// selectElement.innerHTML = 'Select All same classes element using query selectorAll Also use loop';
-console.log(selectAllElement);
-for(var i = 0; i < selectAllElement.length; i ++){
+// // if you select all same class element use query selector
+// // use query selectorAll 
+// var selectAllElement = document.querySelectorAll("h1.intro");
+// // here if two same class in div querySelector Apply only first Element
+// // selectElement.innerHTML = 'Select All same classes element using query selectorAll Also use loop';
+// console.log(selectAllElement);
+// for(var i = 0; i < selectAllElement.length; i ++){
 
-    console.log(selectAllElement[i].innerHTML = 'Select All same classes element using query selectorAll Also use loop');
-}
+//     console.log(selectAllElement[i].innerHTML = 'Select All same classes element using query selectorAll Also use loop');
+// }
