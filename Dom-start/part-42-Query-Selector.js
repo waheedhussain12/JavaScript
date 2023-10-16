6e58deafa01651f8f9db9bbc84a0fbb86e96bb32
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-16 05:12:35 +0500

    done

--- Dom-start/part-42-Query-Selector.js
+++ Dom-start/part-42-Query-Selector.js
@@ -1,9 +1,11 @@
 let body = document.body;
-let container = document.createElement("div"); 
+body.style.cssText = " margin: 0;padding: 5px;box-sizing: border-box;";
+let container = document.createElement("div");
 container.classList.add("divContainer");
-container.style.cssText = 'background-color : pink; display : flex;  align-items: center; justify-content:space-between;padding : 0 40px'
+container.style.cssText =
+  "background-color : pink; display : flex;  align-items: center; justify-content:space-between;padding : 0 40px";
 body.appendChild(container);
-// main div ==> container 
+// main div ==> container
 let logo = document.querySelector(".divContainer");
 let logoText = document.createElement("h2");
 logoText.textContent = "LoGo";
@@ -13,71 +15,44 @@ let webtext = document.createElement("h1");
 webtext.textContent = "website Name";
 websiteName.appendChild(webtext);
 // nav bar
-let navBarDiv = document.body
+let navBarDiv = document.body;
 let navHome = document.createElement("ul");
 navHome.className = "navHome";
-navHome.style.cssText = 'background-color : green; display : flex;  align-items: center; justify-content:end;padding : 13px;gap : 25px'
+navHome.style.cssText =
+  "background-color : green; display : flex;  align-items: center; justify-content:end;padding : 13px;gap : 25px";
 let homeLi = document.createElement("li");
-homeLi.style.cssText = ' list-style-type: none; color: white'
+homeLi.style.cssText = " list-style-type: none; color: white";
 homeLi.textContent = "HOME";
 let aboutLi = document.createElement("li");
-aboutLi.style.cssText = 'list-style-type: none; color: white'
+aboutLi.style.cssText = "list-style-type: none; color: white";
 aboutLi.textContent = "ABOUT US";
 
 let contactLi = document.createElement("li");
-contactLi.style.cssText = ' list-style-type: none; color: white;'
+contactLi.style.cssText = " list-style-type: none; color: white;";
 contactLi.textContent = "CONTACT US";
 navHome.appendChild(homeLi);
 navHome.appendChild(aboutLi);
 navHome.appendChild(contactLi);
 
-
-
 navBarDiv.appendChild(navHome);
 // body
 let mainBody = document.body;
 let mainDiv = document.createElement("div");
-mainDiv.style.background = "orange";
+mainDiv.style.cssText =
+  "display : flex;  align-items: center; justify-content:center;px;gap :20px; margin : 40px 0";
 mainDiv.className = "bodyleft-rigt";
-mainDiv.innerHTML = "wewewweweewr"
+let leftDiv = document.createElement("div");
+leftDiv.textContent =
+  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit in culpaperferendis fuga sunt adipisci porro non quo modi voluptatibus faciliscommodi dignissimos eos, repudiandae nobis dolorum sint. Magni nisi laboreratione saepe eaque, repellendus adipisci nostrum asperiores distinctioDignissimos rerum voluptatum ex laudantium quiFacilis inventore soluta";
+leftDiv.style.cssText = "background-color : green; padding : 100px 8px";
+mainDiv.appendChild(leftDiv);
+let rightDiv = document.createElement("div");
+rightDiv.textContent =
+  " Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit in culpaperferendis fuga sunt adipisci porro non quo modi voluptatibus faciliscommodi dignissimos eos, repudiandae nobis dolorum sint. Magni nisi laboreratione saepe eaque, repellendus adipisci nostrum asperiores distinctioDignissimos rerum voluptatum ex laudantium quiFacilis inventore soluta";
+rightDiv.style.cssText = "background-color : orange;padding : 100px 8px ";
+mainDiv.appendChild(rightDiv);
+
 mainBody.appendChild(mainDiv);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // query selector select element like css
 // var selectElement = document.querySelector("p.intro");
@@ -86,7 +61,7 @@ mainBody.appendChild(mainDiv);
 // console.log(selectElement);
 
 // // if you select all same class element use query selector
-// // use query selectorAll 
+// // use query selectorAll
 // var selectAllElement = document.querySelectorAll("h1.intro");
 // // here if two same class in div querySelector Apply only first Element
 // // selectElement.innerHTML = 'Select All same classes element using query selectorAll Also use loop';
@@ -95,4 +70,3 @@ mainBody.appendChild(mainDiv);
 
 //     console.log(selectAllElement[i].innerHTML = 'Select All same classes element using query selectorAll Also use loop');
 // }
-
