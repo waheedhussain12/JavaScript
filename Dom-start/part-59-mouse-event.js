3983bee3a34aed5b1cfd47ef59ed25dc83badde7
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-17 02:41:13 +0500

    double click event

--- Dom-start/part-59-mouse-event.js
+++ Dom-start/part-59-mouse-event.js
@@ -6,6 +6,9 @@ function myfun (){
     console.log('this is click event');
 }
 // 1 click event
-btn.addEventListener("click",myfun);
+// btn.addEventListener("click",myfun);
 // 2 contextMenu
-btn.addEventListener("contextmenu",myfun);
+// contextmenu works on mouse rightSide button
+// btn.addEventListener("contextmenu",myfun);
+// 3 doubleClick dblclick
+btn.addEventListener("dblclick",myfun);
