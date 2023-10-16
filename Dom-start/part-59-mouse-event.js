d230f1588875e67860a60756f765a8201d1f4ac4
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-17 02:52:45 +0500

    done

--- Dom-start/part-59-mouse-event.js
+++ Dom-start/part-59-mouse-event.js
@@ -6,9 +6,21 @@ function myfun (){
     console.log('this is click event');
 }
 // 1 click event
+// Does fire through keyboard (enter, spacebar keys for example)
 // btn.addEventListener("click",myfun);
 // 2 contextMenu
 // contextmenu works on mouse rightSide button
 // btn.addEventListener("contextmenu",myfun);
 // 3 doubleClick dblclick
 // btn.addEventListener("dblclick",myfun);
+// 4 mousedown 
+//Does not fire through a keyboard
+// btn.addEventListener("mousedown",myfun);
+// 5 mouseup hold relese than show value
+// btn.addEventListener("mouseup",myfun);
+// 6 mouseOver 
+// hover some element value is print
+// btn.addEventListener("mouseover",myfun);
+// 7 mouseOut
+// element out than print
+// btn.adddonEventListener("mouseout",myfun);
