e02a99d25918c75cddc03db3c35b53afef11d22d
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-17 01:57:51 +0500

    done

---
+++ Dom-start/part-58-pageLoad-event.js
@@ -0,0 +1,3 @@
+// DOMContentLoaded the browser fully loaded HTML and completed building the DOM tree.
+// however, it hasn't loaded external resources like styleheets and images. 
+// in this event you can start selecting DOM nodes or initialize thee interface
