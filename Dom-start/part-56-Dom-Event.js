ee9df41025b5e80fcc705412005d9991322b012b
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-16 06:05:29 +0500

    done

--- Dom-start/part-56-Dom-Event.js
+++ Dom-start/part-56-Dom-Event.js
@@ -2,3 +2,7 @@
 // when a user clicks the mouse
 // when a web page has loaded
 // when an image has been loaded
+// when the mouse moves over an element
+// when an input field is changed
+// when a HTML form is submitted
+// when a user strokes a key
