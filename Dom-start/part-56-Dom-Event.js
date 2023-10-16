645e85155444b4104891f7243eee030637d4b678
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-16 06:11:05 +0500

    done

--- Dom-start/part-56-Dom-Event.js
+++ Dom-start/part-56-Dom-Event.js
@@ -6,3 +6,12 @@
 // when an input field is changed
 // when a HTML form is submitted
 // when a user strokes a key
+
+// note there are two types of register an Events
+// 1 inline evenets
+// 2 
+
+
+function btnClicked(){
+    alert('clicked');
+}
