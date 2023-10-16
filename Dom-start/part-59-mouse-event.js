40b3697b87f3d277ac9e1a376132d190dc36e802
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-17 02:34:37 +0500

    done

--- Dom-start/part-59-mouse-event.js
+++ Dom-start/part-59-mouse-event.js
@@ -1,1 +1,9 @@
 let btn = document.getElementById("mouse");
+// external function
+function myfun (){
+    let heading = document.getElementById("div");
+    heading.textContent = "this is heading added from js function"
+    console.log('this is click event');
+}
+// click event
+btn.addEventListener("click",)
