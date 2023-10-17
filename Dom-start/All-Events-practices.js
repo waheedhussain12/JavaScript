3e50a733a5b72ea74114af26be7e452fd7272c22
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-17 11:07:49 +0500

    done

--- Dom-start/All-Events-practices.js
+++ Dom-start/All-Events-practices.js
@@ -1,1 +1,20 @@
 // 1 inline Event
+// for inline make function
+function inlineFun(){
+   
+let heading = document.querySelector(".heading");
+heading.innerHTML = "this is Inline Event";
+};
+// 2 AddEventListener
+let addlistenBtn = document.querySelector(".addlistenBtn");
+// apply click Event
+addlistenBtn.addEventListener("click",addEventFun);
+// make function 
+function addEventFun(n , n1){
+    let pTag = document.querySelector(".pTag");
+    let firstName = "Waheed"; 
+    let lastName = "Hussain";
+    pTag.innerHTML = `${firstName} ${lastName}`
+
+}
+
