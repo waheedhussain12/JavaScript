13719118b57110ffa17d249e1778e701e08f8f92
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-22 11:36:44 +0500

    done

---
+++ Dom-start/part-67-timeOut-timeInterval.js
@@ -0,0 +1,28 @@
+// time out and time interval
+// time out means delay particular time
+
+let timeOut = setTimeout(myfun,3000);
+// timeout return a id yu can store in variable as well 
+// you can make condition as well
+function myfun(){
+    console.log("Print every three seconds of delay");
+    let body = window.document.body;
+    body.style.background = "orange"
+
+}
+// you can clear as well
+// clearTimeout(timeOut);
+
+// timeInterval
+let timeinterval = setInterval(myfun2,2000)
+function myfun2(){
+    console.log("Print every two sec of time");
+    // let body = window.document.body;
+    // body.style.background = "orange"
+
+}
+let btn = document.querySelector("#btn");
+btn.addEventListener("click",function(){
+
+clearInterval(timeinterval);
+})
