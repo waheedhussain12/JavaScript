b75c660705309e8cc9c76537cf733fa03320a674
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-22 10:52:46 +0500

    done

--- Dom-start/part-66-window.js
+++ Dom-start/part-66-window.js
@@ -13,3 +13,34 @@
  let outerHeightCheck = window.outerHeight;
  let outerWidthCheck = window.outerWidth;
 console.log(`this is outer Height ${outerHeightCheck} and this is outer Width ${outerWidthCheck}`);
+
+// how to open new Window or pop window
+
+let googleButton = window.document.querySelector("#googlebtn");
+let googleButton2 = window.document.querySelector("#googlebtn");
+let closebtn = window.document.querySelector("#closebtn");
+let win;
+googleButton.addEventListener("click",function(){
+    // tabs are open
+    // three parameters inSide the open method
+    // 1 url 2 name 3 features
+    let url = "https://www.google.com"; // set which site or link to open
+    let url2 = "https://www.yahoo.com"
+    let target = "yahoo"; // name set  
+    let features = "height = 400, width = 400"; // add features like 
+    // height width left screen x or right screen y
+// if you want to open new tab use featurs
+// window.open(url , target , features)
+   win = window.open(url2,target);
+});
+
+googleButton2.addEventListener("click",function(){
+window.open("https://www.yahoo.com/news/never-vote-biden-muslim-americans-191319506.html","yahoo");
+
+})
+// store a variable which tab you are closing
+// make global variable
+closebtn.addEventListener("click",function(){
+  win.close();
+    })
+
