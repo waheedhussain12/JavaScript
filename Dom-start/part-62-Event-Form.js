4957adab5ad942a0df9c61fe0918b2e0384d0d56
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-22 08:40:48 +0500

    BOM

--- Dom-start/part-62-Event-Form.js
+++ Dom-start/part-62-Event-Form.js
@@ -6,15 +6,15 @@ myInput.addEventListener("blur", myBlurFunction);
 // focus means yu can type on input field
 // blur means yu can not type on input
 // myInput.addEventListener("change", function () {
-//   // change event show us what user typing
-//   // once user type than show us
-//   // get value
+  // change event show us what user typing
+  // once user type than show us
+  // get value
 //   console.log(this.value);
 // });
 // myInput.addEventListener("input", function () {
-//   // input event show us like live value
-//   // anything user typing using input Event we can access
-//   // get value
+  // input event show us like live value
+  // anything user typing using input Event we can access
+  // get value
 //   console.log(this.value);
 // });
 function myFocusFunction() {
---
+++ Dom-start/part-65-BOM.js
@@ -0,0 +1,38 @@
+// BOM 
+// the Browser Object Model (BOM) is used to interact with the browser
+// the window object represets a window in browser
+// an object of window is created automatically by the browser
+// All global JavaScript objects, functions , and variables with the var 
+// keyword automatically become members of the window object
+
+// Global variables are properties of the window object
+// Global functions are methods of the window object
+
+var firstName = "Waheed"; // normal declared
+// 1 this is variable
+// 2 this is also window' property
+// we know that access any property with . notation
+// i want to check this is window object's property or not
+
+console.log(window.firstName);
+// this is window object
+
+// but let and const are not a window's property
+// you got undefined bcz let and const are not a property
+let firstName1 = "Waheed"; 
+console.log(window.firstName1);
+
+// window is a default object
+
+// check function is a window property or not
+
+function sayMyName(){
+    console.log("waheed hussain");
+}
+
+// calling function
+sayMyName(); // bcz window is a default object automatically understand
+// behind the scene is
+window.sayMyName();
+// here function is a property or method of window 
+// so functions , objects and variables with var keywords is all window object's properties
