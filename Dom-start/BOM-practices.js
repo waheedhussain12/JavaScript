a34ff874c4eec6c7d8f7ab44b6c9d5854588b1aa
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-25 13:48:25 +0500

    done

--- Dom-start/BOM-practices.js
+++ Dom-start/BOM-practices.js
@@ -30,4 +30,52 @@ console.log(window.innerWidth);
 console.log(window.innerHeight);
 console.log(window.outerHeight);
 console.log(window.outerWidth);
+// global variable
+let win;
+// window Open Function
+let btn = document.getElementById("open");
+btn.addEventListener("click",function(){
+    let url = "https://www.google.com";
+    // but i want to open new tab in brower use features property
+    let features = "width = 200 , height = 200";
+    let target = "newtab" // for new window
+  win =  window.open(url,target,features);
+})
 
+btn.addEventListener("click",function(){
+
+   let url2 =  "https://www.yahoo.com";
+  
+window.open(url2, "newtab");
+
+})
+
+let btn2 = document.getElementById("open1");
+btn2.addEventListener("click",function(){
+
+win.close();
+ 
+ })
+ 
+ // settime out
+ let timeOut = setTimeout(timeFun,3000);
+ function timeFun(){
+    console.log("waheed hussain");
+
+    
+ }
+
+
+
+ 
+ // time Interval
+ let timeinterval = setInterval(timeintervalFun,1000);
+ function timeintervalFun(){
+    console.log("waheed hussain");
+ }
+ let clearBtn2 = document.getElementById("time");
+clearBtn2.addEventListener("click",function(){
+// pass variable which holds the settimeout function
+clearInterval(timeinterval);
+ 
+ })
---
+++ Dom-start/part-68-69-70-location-navigater-screen-objects.js
@@ -0,0 +1,61 @@
+// Locations methods and properties
+
+let href = location.href;
+console.log(href);
+let pathName = window.location.pathname;
+console.log(pathName);
+let protoCol = location.protocol;
+console.log(protoCol);
+let Port = location.port;
+console.log(Port);
+
+// also redirect
+let button = document.getElementById("btn");
+button.addEventListener("click", function(){
+
+// there are three types of redirect
+// 1 location
+// window.location = "https://www.google.com";
+// 2 loaction href
+// window.location.href = "https://www.yahoo.com";
+// 3 assign() method
+window.location.assign("https://www.yahoo.com");
+// these methods and properties provides us back button
+// these all works same but assign is a methods and other two are properties
+// 4 replace
+// does not has back button means dose not store history
+// location.replace("https://www.yahoo.com");
+// 5 reload
+// you can also reload the page using location reload method
+location.reload();
+
+
+
+
+})
+
+
+
+// Navigatar object
+// navigatar give us imformation about browser
+console.log(navigator.appName);
+console.log(navigator.appCodeName);
+console.log(navigator.appVersion);
+console.log(navigator.cookieEnabled);
+console.log(navigator.userAgent);
+console.log(navigator.platform);
+console.log(navigator.userAgent);
+console.log(navigator.javaEnabled);
+
+
+
+
+//Screen Object 
+// imformation about screens 
+// which device opne in our website
+console.log(screen.width);
+console.log(screen.colorDepth);
+console.log(screen.height);
+console.log(screen.pixelDepth);
+console.log(screen.orientation);
+console.log(screen.availHeight);
