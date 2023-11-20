7d7ecfae6b42137e359c1c76475b5edc5c0d6d5a
waheedhussain <tommyshelby811@gmail.com>
2023-11-20 19:27:00 +0500

    done

--- Dom-start/All-Events-practices.js
+++ Dom-start/All-Events-practices.js
@@ -20,6 +20,7 @@ function addEventFun(){
 // AddEventListener second way
 // AddEventListener second way
 // AddEventListener second way
+// AddEventListener second way
 // make function with no name or direct pass
 let addlistenBtnWithNoName = document.querySelector(".addlistenBtnWithNoName");
 addlistenBtnWithNoName.addEventListener("click",function(){
