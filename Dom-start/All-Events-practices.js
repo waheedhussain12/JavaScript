7889a77f90f2573d49e589a0889b7f30b08a3a1a
waheedhussain <tommyshelby811@gmail.com>
2023-11-20 19:28:54 +0500

    done

--- Dom-start/All-Events-practices.js
+++ Dom-start/All-Events-practices.js
@@ -22,6 +22,7 @@ function addEventFun(){
 // AddEventListener second way
 // AddEventListener second way
 // AddEventListener second way
+// AddEventListener second way
 // AddEventListener second way// AddEventListener second way
 // AddEventListener second way
 // AddEventListener second way
